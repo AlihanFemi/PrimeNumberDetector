63af0c643b117c82f080fcd95a3204a94fc44c4c
AlihanFemi <147508857+AlihanFemi@users.noreply.github.com>
2023-12-06 17:41:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberDetector
+{
+    internal class Program
+    {
+        static bool IsPrime(int num)
+        {
+            if (num < 1) 
+                return false;
+            var digits = new List<int>() {2, 3, 4, 5, 6, 7, 8, 9};
+            foreach(int digit in digits)
+            {
+                if (num % digit == 0 && digit != num)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPrime(2));
+            Console.WriteLine(IsPrime(3));
+            Console.WriteLine(IsPrime(5));
+            Console.WriteLine(IsPrime(7));
+            Console.WriteLine(IsPrime(11));
+            Console.WriteLine(IsPrime(13));
+            Console.WriteLine(IsPrime(17));
+            Console.WriteLine(IsPrime(19));
+            Console.WriteLine(IsPrime(-2));
+            Console.WriteLine(IsPrime(1234));
+        }
+    }
+}
